6a25fa4a0a2b8abdc416153d0bfd907db6a691ab
MaxBilyk1401 <black.bro1401@gmail.com>
2023-06-08 02:44:37 +0300

    Fix

--- PlaceholderSession/Views/UsersVeiwController.swift
+++ PlaceholderSession/Views/UsersVeiwController.swift
@@ -81,7 +81,7 @@ extension UsersVeiwController {
         return cell
     }
     
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        <#code#>
-//    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
